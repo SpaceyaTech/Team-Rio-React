ba6257ed541212f11d94c92977755d934370ce19
Nyandika Donald <donald.nyandika@gmail.com>
2022-11-02 14:48:13 +0000

    Init tailiwindcss

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,8 @@
-import '../src/assets/App.css';
-import logo from '../src/assets/logo.svg';
 import Home from './pages/SamplePage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
       <Home />
     </div>
   );
