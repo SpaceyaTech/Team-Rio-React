83c5d49dd9fc8927bc12c1307728a8dab4aa8d04
Nyandika Donald <donald.nyandika@gmail.com>
2022-12-07 21:04:40 +0000

    Add blog filter section

---
+++ src/components/Button/BlogFilterButton.tsx
@@ -0,0 +1,21 @@
+export interface BlogFilterButtonProps {
+  category: string;
+  isActive: boolean;
+  handleClick: (category: string) => void;
+}
+
+export default function BlogFilterButton(props: BlogFilterButtonProps) {
+  const style = props.isActive
+    ? 'bg-black hover:bg-gray-700 text-white'
+    : 'bg-gray-200 hover:bg-gray-100 text-black';
+
+  return (
+    <button
+      type="button"
+      className={`${style} font-bold py-2 px-4 ml-4 rounded-3xl`}
+      onClick={() => props.handleClick(props.category)}
+    >
+      {props.category}
+    </button>
+  );
+}
