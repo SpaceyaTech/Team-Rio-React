ea9f875782cc42a157e8c280e2b50b6ae9737f60
Nyandika Donald <donald.nyandika@gmail.com>
2022-12-08 15:49:44 +0000

    Add tests for custom and categoryfilter buttons (#29)
    * Add custom and category filter button tests
    
    * Fix linting issues

---
+++ src/__tests__/components/Button/CustomButton.test.tsx
@@ -0,0 +1,17 @@
+import { render, screen } from '@testing-library/react';
+import CustomButton from '../../../components/Button/CustomButton';
+
+test('renders text and color passed in props', () => {
+  const bgColor = 'gray';
+  const buttonText = 'This is a button';
+  const textColor = 'white';
+
+  render(
+    <CustomButton text={buttonText} color={bgColor} textColor={textColor} />
+  );
+  const button = screen.getByRole('button');
+
+  expect(button).toHaveClass('bg-gray-900');
+  expect(button).toHaveTextContent(buttonText);
+  expect(button).toHaveClass('text-white');
+});
